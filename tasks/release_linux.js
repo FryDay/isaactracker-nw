719131022751490717a2ec76bb48d453ce6ccf9b
Jordan Day <jordan@scottsmarketplace.com>
2017-01-03 14:35:00 -0600

    Use nw-builder

--- tasks/release_linux.js
+++ tasks/release_linux.js
@@ -1,74 +1,17 @@
 'use strict';
 
-var Q = require('q');
-var gulpUtil = require('gulp-util');
-var childProcess = require('child_process');
-var jetpack = require('fs-jetpack');
-var utils = require('./utils');
+var gulp = require('gulp');
+var NwBuilder = require('nw-builder');
 
-var projectDir;
-var releasesDir;
-var packName;
-var packDir;
-var tmpDir;
-var readyAppDir;
-var manifest;
-
-var init = function () {
-    projectDir = jetpack;
-    tmpDir = projectDir.dir('./tmp', { empty: true });
-    releasesDir = projectDir.dir('./releases');
-    manifest = projectDir.read('app/package.json', 'json');
-    packName = manifest.name + '_' + manifest.version;
-    packDir = tmpDir.dir(packName);
-    readyAppDir = packDir.cwd('opt', manifest.name);
-
-    return Q();
-};
-
-var copyRuntime = function () {
-    return projectDir.copyAsync('node_modules/nw/nwjs', readyAppDir.path(), { overwrite: true });
-};
-
-var copyBuiltApp = function () {
-    return projectDir.copyAsync('build', readyAppDir.path(), { overwrite: true });
-};
-
-var packToFile = function () {
-    var deferred = Q.defer();
-
-    var fileName = packName + ".nw";
-    var path = releasesDir.path(fileName);
-
-    gulpUtil.log('Creating package...');
-
-    // Counting size of the app in KiB
-    var appSize = Math.round(readyAppDir.inspectTree('.').size / 1024);
-
-    // Build the package...
-    childProcess.exec('cat ' + nw.findPath() + ' ' + path + ' > ' + packDir.path() + ' && chmod +x ' + packDir.path(),
-        function (error, stdout, stderr) {
-            if (error || stderr) {
-                console.log("ERROR while building linux package:");
-                console.log(error);
-                console.log(stderr);
-            } else {
-                gulpUtil.log('package ready!', path);
-            }
-            deferred.resolve();
-        });
-
-    return deferred.promise;
-};
-
-var cleanClutter = function () {
-    return tmpDir.removeAsync('.');
-};
+var nw = new NwBuilder({
+    files: ['./app/**'],
+    flavor: 'normal',
+    buildDir: 'releases',
+    platforms: ['linux64']
+});
 
 module.exports = function () {
-    return init()
-    .then(copyRuntime)
-    .then(copyBuiltApp)
-    .then(packToFile)
-    .then(cleanClutter);
-};
+  return nw.build().catch(function (error) {
+    console.error(error);
+  });
+}
